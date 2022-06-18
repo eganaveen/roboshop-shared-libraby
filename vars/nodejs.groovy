5495ea9bc5db1db41e1366dfce4efad3d25d0b20
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-18 13:14:15 +0530

    jslint in cart

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -15,7 +15,7 @@ def call(){
             stage('Lint Checks'){
                 steps{
                     script{
-                        nodejs.lintChecks()
+                        lintChecks()
                     }
                 }
             }
