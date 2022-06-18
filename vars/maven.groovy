e115c5759d17c0bb47236d72ca1185a097838860
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-18 13:36:33 +0530

    maven checkstyles for shipping

---
+++ vars/maven.groovy
@@ -0,0 +1,25 @@
+def lintChecks(){
+    sh '''
+      #~/node_modules/jslint/bin/jslint.js server.js
+      mvn checkstyle:check
+      #echo lint checks for ${COMPONENT}
+    '''
+}
+
+def call(){
+    pipeline{
+        agent any
+
+        stages{
+
+            //lint checks
+            stage('Lint Checks'){
+                steps{
+                    script{
+                        lintChecks()
+                    }
+                }
+            }
+        }//End of stages
+    }
+}
