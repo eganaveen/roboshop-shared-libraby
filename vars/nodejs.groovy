6ed487876af7b49e92eb511010b0a90cc7c530a6
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-18 15:54:37 +0530

    sonarqube

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,10 +5,19 @@ def lintChecks(){
     '''
 }
 
+def sonarcheck(){
+    sh '''
+      sonar-scanner -Dsonar.host.url=http://172.31.26.97:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
+    '''
+}
+
 def call(){
     pipeline{
         agent any
 
+        environment{
+            SONAR = credentials('SONAR')
+        }
         stages{
 
             //lint checks
@@ -19,6 +28,16 @@ def call(){
                     }
                 }
             }
+
+            //Sonar qube quality check
+            stage('sonarqube'){
+                steps{
+                    script{
+                        sonarcheck()
+                    }
+                }
+            }
+
         }//End of stages
     }
 }
