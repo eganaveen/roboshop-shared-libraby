5cae17dafef7aef2f33b2a3cbbca6918f388d1ae
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-20 21:10:45 +0530

    prepare and upload artifact

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -65,7 +65,7 @@ def call(){
                 steps {
                     script{
                         def UPLOAD_STATUS=sh(returnStdout: true, script: "curl -s -L http://54.92.218.237:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip")
-                        output UPLOAD_STATUS
+                        print UPLOAD_STATUS
                     }
                 }
             }
