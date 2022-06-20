1d6ddd80f50794cfeb7ff6ca0583e369b69bb20a
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-20 23:21:28 +0530

    hhbnhndssd

--- vars/common.groovy
+++ vars/common.groovy
@@ -86,7 +86,7 @@ def artifacts(){
             }
         }
         stage('Upload artifact to nexus'){
-            withCredentials([usernamePassword(credentialsId:'NEXUS', passwordVariable: '{NEXUS_PSW}', usernameVariable: '${NEXUS_USR}')]){
+            withCredentials([usernamePassword(credentialsId:'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]){
                 sh'''
                     curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.21.99:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
                 '''
