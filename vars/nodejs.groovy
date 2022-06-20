b1b5da685111a24a0bedea198905ff6ec72c020b
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-20 21:56:22 +0530

    prepare and upload artifact

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,9 +1,9 @@
-env.APP_TYPE="nodejs"
+env.APP_TYPE = "nodejs"
 def call() {
     node {
         common.lintChecks()
         env.ARGS = "-Dsonar.sources=."
-        common.sonarcheck()
+        common.sonarCheck()
         common.testCases()
     }
 }
