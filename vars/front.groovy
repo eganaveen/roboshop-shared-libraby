82ee9fe1544483426204e8f9181c2a2bc49b5401
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-23 12:57:31 +0530

    maven artifact create and upload

--- vars/front.groovy
+++ vars/front.groovy
@@ -2,6 +2,7 @@ def call() {
     node {
         sh 'rm -rf *'
         git branch: "main", url: "https://github.com/eganaveen/${COMPONENT}"
+        sh 'ls -l'
         env.app_type="front"
         sh 'echo this is from frontend'
         common.lintChecks()
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,6 +2,7 @@ def call() {
     node {
         sh 'rm -rf *'
         git branch: "main", url: "https://github.com/eganaveen/${COMPONENT}"
+        sh 'ls -l'
         env.APP_TYPE = "nodejs"
         sh 'echo this is from nodejs'
         common.lintChecks()
