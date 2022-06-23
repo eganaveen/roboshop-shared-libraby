896889db072fc4ddff55de05efe5753bf7c8e016
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-23 12:41:08 +0530

    maven artifact create and upload

--- vars/front.groovy
+++ vars/front.groovy
@@ -2,7 +2,7 @@ def call() {
     node {
         sh 'rm -rf *'
         git branch: "main", url: "https://github.com/eganaveen/${COMPONENT}"
-        env.APP_TYPE = "front"
+        env.APP_TYPE = "golang"
         common.lintChecks()
         env.ARGS = "-Dsonar.sources=."
         common.sonarCheck()
