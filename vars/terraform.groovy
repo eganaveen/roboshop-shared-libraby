9241fd054a231ac2c80b4baa7d8cb4e1baef32f0
Ega Naveen Reddy <enreddy777@gmail.com>
2022-06-28 17:09:17 +0530

    jenkinsfile

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -3,18 +3,20 @@ def call() {
         choice(choices: ['dev', 'prod'], description: "Pick ENV", name: "ENV")
     }
     node {
-        git branch: "main", url: "https://github.com/eganaveen/${REPONAME}"
-        stage('Terrafile init'){
-            sh 'terrafile -f ${ENV}-env/Terrafile'
-        }
-        stage('Terraform init'){
-            sh 'terraform init -backend-config=${ENV}-env/${ENV}-backend.tfvars'
-        }
-        stage('Terraform plan'){
-            sh 'terraform plan -var-file=${ENV}-env/${ENV}.tfvars'
-        }
-        stage('Terraform plan'){
-            sh 'terraform apply -var-file=${ENV}-env/${ENV}.tfvars --auto-approve'
+        ansiColor('xterm') {
+            git branch: "main", url: "https://github.com/eganaveen/${REPONAME}"
+            stage('Terrafile init'){
+                sh 'terrafile -f ${ENV}-env/Terrafile'
+            }
+            stage('Terraform init'){
+                sh 'terraform init -backend-config=${ENV}-env/${ENV}-backend.tfvars'
+            }
+            stage('Terraform plan'){
+                sh 'terraform plan -var-file=${ENV}-env/${ENV}.tfvars'
+            }
+            stage('Terraform plan'){
+                sh 'terraform apply -var-file=${ENV}-env/${ENV}.tfvars --auto-approve'
+            }
         }
     }
 }
